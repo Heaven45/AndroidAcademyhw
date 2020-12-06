e55f291881a4f3b0c7d5d14ae8422a9a0d3b599c
fedulov-s <fedulov-s@yoomoney.ru>
2020-12-06 12:24:40 +0300

    Homework 3. Fragments

--- app/src/main/java/com/example/androidacademyhw/MainActivity.kt
+++ app/src/main/java/com/example/androidacademyhw/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.androidacademyhw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentMoviesClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .add(R.id.fragment_movies_list, FragmentMoviesList())
+                    .commit()
+        }
+
+    }
+
+    override fun onMovieDetailsClicked() {
+        supportFragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.fragment_movies_list, FragmentMoviesDetails())
+                .commit()
+    }
+
+    fun onBackFragmentClicked() {
+        supportFragmentManager.beginTransaction()
+                .remove(FragmentMoviesList())
+                .commit()
+    }
+    
+    interface FragmentClickListener {
+        fun onMovieDetailsClicked()
+        fun onBackFragmentClicked()
     }
 }
+
+
+
+
+
